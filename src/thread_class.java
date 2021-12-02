56ad0d9615d6e1ddf7727e096219f07d28cc6b33
HP <shubhi1488@gmail.com>
2021-12-02 19:09:33 +0530

    Thread

---
+++ src/thread_class.java
@@ -0,0 +1,21 @@
+/*
+Create two threads and assign names "Scooby" and "Shaggy" to the two threads.Display both the thread names.
+ */
+public class thread_class {
+    public static void main(String[] args) {
+        Thread t1=new Thread("Scooby")
+        {
+            public void run(){
+            System.out.println("I'm "+Thread.currentThread().getName());
+        }
+        };
+        Thread t2=new Thread("Shaggy"){
+            public void run(){
+                System.out.println("I'm "+Thread.currentThread().getName());
+            }
+
+        };
+        t1.start();
+        t2.start();
+    }
+}
